f0b9deb4125daed14c81cbc0b2ce3b4a62b96d94
AndriiAvramenk <72814303+AndriiAvramenk@users.noreply.github.com>
2023-03-20 20:34:53 +0200

    Wait, Ohio back?

---
+++ Ohio☠️/Project Oklahoma/Oklahoma.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string str = "Hello World!";
+            Console.WriteLine(str)
+        }
+    }
+}
